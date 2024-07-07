2d6a8eb75c1cd70b142824ab65cd9cacfffc55b1
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-07 21:22:42 +0200

    08. create the getPokemons() method

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -10,4 +10,16 @@ export class PokemonService {
   constructor() {
     this.nextUrl = 'https://pokeapi.co/api/v2/pokemon?offset=00&limit=20';
   }
+
+  getPokemons() {
+    const url = this.nextUrl;
+
+    if(url) {
+
+    }
+
+    return null;
+
+  }
+
 }
