3fef51dbce7ebb97baaea569835a123726a014de
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-07 21:50:07 +0200

    10. implement the getPokemons() method

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {CapacitorHttp, HttpResponse} from "@capacitor/core";
+import {Pokemon} from "../models/pokemon";
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +18,41 @@ export class PokemonService {
 
     if(url) {
 
+      const options = {
+        url,
+        headers: {},
+        params: {}
+      }
+
+      return CapacitorHttp.get(options).then(async (response) => {
+        let pokemons: Pokemon[] = [];
+
+        console.log(response);
+
+        if (response.data) {
+          const listPokemons = response.data.results;
+          this.nextUrl = response.data.next;
+          const promises: Promise<HttpResponse>[] = [];
+
+          for (let index = 0; index < listPokemons.length; index++) {
+            const pokemon = listPokemons[index];
+            const urlPokemon = pokemon.url;
+            const options = {
+              url: urlPokemon,
+              headers: {},
+              params: {}
+            }
+            promises.push(CapacitorHttp.get(options));
+          }
+          await Promise.all(promises).then((responses) => {
+            console.log(responses);
+          });
+        }
+        return pokemons;
+      })
     }
 
     return null;
 
   }
-
 }
