e24ebecef031e221d8cd7429bfe7565bf529e306
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-11 11:45:22 +0200

    15. create pokemons array and implement morePokemon()

--- src/app/pages/list-pokemons/list-pokemons.page.ts
+++ src/app/pages/list-pokemons/list-pokemons.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import {PokemonService} from "../../services/pokemon.service";
+import {Pokemon} from "../../models/pokemon";
 
 @Component({
   selector: 'app-list-pokemons',
@@ -13,11 +14,26 @@ import {PokemonService} from "../../services/pokemon.service";
 })
 export class ListPokemonsPage implements OnInit {
 
+  public pokemons: Pokemon[];
+
   constructor(
     private pokemonService: PokemonService
   ) { }
 
   ngOnInit() {
+    this.morePokemon();
+  }
+
+  morePokemon() {
+    const promise = this.pokemonService.getPokemons();
+
+    if (promise) {
+      promise.then((result: Pokemon[]) => {
+        console.log(result);
+        this.pokemons = this.pokemons.concat(result);
+        console.log(this.pokemons);
+      })
+    }
   }
 
 }
